80491546960bdfb1a67ddf782b8e6d706aa5ce52
zuryansh <87305245+zuryansh@users.noreply.github.com>
2026-05-27 03:08:05 +0530

    Added Basic Player Movement

--- Assets/RoomGenerator.cs
+++ Assets/RoomGenerator.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class RoomGenerator : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Camera cam;
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector2 movementVector;
+    Vector3 towardsMouse;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    public void Move()
+    {
+        // get the maxSpeed
+        Vector2 targetSpeed = movementVector * moveSpeed;
+
+        // get the difference b/w current and max speed
+        Vector2 speedDif = targetSpeed - rb.linearVelocity;
+        rb.AddForce(speedDif, ForceMode2D.Impulse); // impulse feels more snappy but FORCE feels more floaty
+
+    }
+
+}
